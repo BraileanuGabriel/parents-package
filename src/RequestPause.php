5614413aa3f006b670a6ac5be29b3f7267b493e7
BraileanuGabriel <gbraileanu15@gmail.com>
2022-06-27 15:55:18 +0300

    testing

--- src/RequestPause.php
+++ src/RequestPause.php
@@ -3,48 +3,16 @@
 namespace func\HelloWorld;
 
 use GuzzleHttp\HandlerStack;
-use GuzzleHttp\MessageFormatter;
 use GuzzleHttp\Middleware;
 
 class RequestPause
 {
-    public function createHandlerStack($tries = 5): HandlerStack
+    public function createHandlerStack($tries = 5)
     {
         $stack = HandlerStack::create();
         $stack->push(Middleware::retry(function () use($tries) {return $tries;}, $this->retryDelay()));
-        return $this->createLoggingHandlerStack($stack);
     }
 
-    protected function createLoggingHandlerStack(HandlerStack $stack): HandlerStack
-    {
-        $messageFormats = [
-            '{method} {uri} HTTP/{version}',
-            'HEADERS: {req_headers}',
-            'BODY: {req_body}',
-            'RESPONSE: {code} - {res_body}',
-        ];
-        foreach ($messageFormats as $messageFormat) {
-            // We'll use unshift instead of push, to add the middleware to the bottom of the stack, not the top
-            $stack->unshift(
-                $this->createGuzzleLoggingMiddleware($messageFormat)
-            );
-        }
-
-        return $stack;
-    }
-
-    protected function createGuzzleLoggingMiddleware(string $messageFormat): callable
-    {
-        return Middleware::log( null,
-            new MessageFormatter($messageFormat)
-        );
-    }
-    
-    /**
-     * delay 1s 2s 3s 4s 5s ...
-     *
-     * @return callable
-     */
     protected function retryDelay()
     {
         $config = config('job_pause.pause_job_delay');
