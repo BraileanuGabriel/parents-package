cdbad4c9cdbf780fbf4df60665ea97533dc3d537
BraileanuGabriel <gbraileanu15@gmail.com>
2022-06-27 16:02:33 +0300

    testing

--- src/RequestPause.php
+++ src/RequestPause.php
@@ -2,17 +2,67 @@
 
 namespace func\HelloWorld;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
 use GuzzleHttp\Middleware;
+use Psr\Log\LoggerInterface;
 
 class RequestPause
 {
-    public function createHandlerStack($tries = 5)
+    protected $logger;
+    protected $client;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+        $this->client = new Client([
+            'handler' => $this->createHandlerStack(),
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+            'verify' => !config('services.w_parents.env')
+        ]);
+    }
+
+    public function createHandlerStack($tries = 5): HandlerStack
     {
         $stack = HandlerStack::create();
         $stack->push(Middleware::retry(function () use($tries) {return $tries;}, $this->retryDelay()));
+        return $this->createLoggingHandlerStack($stack);
     }
 
+    protected function createLoggingHandlerStack(HandlerStack $stack): HandlerStack
+    {
+        $messageFormats = [
+            '{method} {uri} HTTP/{version}',
+            'HEADERS: {req_headers}',
+            'BODY: {req_body}',
+            'RESPONSE: {code} - {res_body}',
+        ];
+        foreach ($messageFormats as $messageFormat) {
+            // We'll use unshift instead of push, to add the middleware to the bottom of the stack, not the top
+            $stack->unshift(
+                $this->createGuzzleLoggingMiddleware($messageFormat)
+            );
+        }
+
+        return $stack;
+    }
+
+    protected function createGuzzleLoggingMiddleware(string $messageFormat): callable
+    {
+        return Middleware::log( null,
+            new MessageFormatter($messageFormat)
+        );
+    }
+
+    /**
+     * delay 1s 2s 3s 4s 5s ...
+     *
+     * @return callable
+     */
     protected function retryDelay()
     {
         $config = config('job_pause.pause_job_delay');
