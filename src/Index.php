88093d237cf8e2ceb8f7945c8ed691886984f491
BraileanuGabriel <gbraileanu15@gmail.com>
2022-06-27 10:24:25 +0300

    go

--- src/Index.php
+++ src/Index.php
@@ -2,10 +2,33 @@
 
 namespace func\HelloWorld;
 
+use App\Job;
+use Illuminate\Support\Facades\Cache;
+
 class Index
 {
-    public function greet($greet = "Hello World")
-    {
-        return $greet;
+    public function check($queue = 'default'){
+        $attempts = Job::where('queue', $queue)->sum('attempts');
+        info($queue);
+        if($attempts){
+            $delay = $this->findDelay($attempts);
+            $this->pause($queue, $delay);
+        }
+    }
+
+    public function findDelay($attempts){
+        $config = config('app.pause_job_delay');
+        switch ($attempts){
+            case 1: return $config[1];
+            case 2: return $config[2];
+            case 3: return $config[3];
+            case 4: return $config[4];
+            case 5: return $config[5];
+            case $attempts>5: return $config[6];
+        }
+    }
+
+    public function pause($queue, $delay){
+        Cache::put('pause_'.$queue.'_queue', $delay, $delay);
     }
 }
