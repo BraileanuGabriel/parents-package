ac19a1c83c9aac4eb966d6d4b73988ca1087be86
BraileanuGabriel <gbraileanu15@gmail.com>
2022-06-29 15:59:19 +0300

    using DB not model for jobs

--- src/JobPause.php
+++ src/JobPause.php
@@ -9,8 +9,12 @@ class JobPause
 {
     public function check($thisJob, $queue = 'default'){
         if($thisJob->attempts() == 0) return;
-        $attempts = DB::table('jobs')->where('queue', $queue)->sum('attempts');
-        if($attempts){
+
+        if($thisJob->attempts() != 0 && !cache()->get('pause_'.$queue.'_queue')){
+            return;
+        }
+
+        if($attempts = DB::table('jobs')->where('queue', $queue)->sum('attempts')){
             $delay = $this->findDelay($attempts);
             $this->pause($queue, $delay);
         }
