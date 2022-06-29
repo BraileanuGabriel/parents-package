2c0adb987a9a8caf81a7747b64dc9ac0d72ebeb8
BraileanuGabriel <gbraileanu15@gmail.com>
2022-06-29 12:27:08 +0300

    refactoring file placement

--- src/JobPause.php
+++ src/JobPause.php
@@ -22,7 +22,7 @@ class JobPause
     }
 
     public function pause($queue, $delay){
-        info($queue, $delay);
+        info($queue.' '.$delay);
         Cache::put('pause_'.$queue.'_queue', $delay, $delay);
         info(Cache::get('pause_'.$queue.'_queue'));
     }
